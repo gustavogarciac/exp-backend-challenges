ee555dcc89649644cc4150ed0e075ee2516549f3
gustavo <gustavogarciaci1@gmail.com>
2024-08-12 21:39:02 -0300

    feat: docker compose and prisma setup

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,6 +4,12 @@ import z from 'zod'
 
 const envSchema = z.object({
   API_PORT: z.coerce.number().default(3333),
+  NODE_ENV: z.enum(['development', 'production']).default('development'),
+  BOOKY_DATABASE_URL: z.string(),
+  BOOKY_DATABASE_NAME: z.string().default('booky'),
+  BOOOKY_DATABASE_USER: z.string(),
+  BOOKY_DATABASE_PASSWORD: z.string(),
+  DATABASE_URL: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
