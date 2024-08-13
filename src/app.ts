1a61ad2399d1ff9dccd0fcdcaf5f91ac74c12928
gustavo <gustavogarciaci1@gmail.com>
2024-08-12 21:50:54 -0300

    feat: swagger config

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,30 @@
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import fastify from 'fastify'
-import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Booky API',
+      description:
+        'This is the API documentation for the Booky API. An API for managing books.',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
